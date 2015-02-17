41f70b1e648ed4cee6a44a1f94d3f7c7dc78d743
Sergio-Vazquez <svazquezpelaez@gmail.com>
2015-02-17 22:17:21 +0100

    Añado csv con el precio de las gasolinas y actualizo el main.

--- main.R
+++ main.R
@@ -1,6 +1,10 @@
 library(quantmod)
 library(ggplot2)
+library(data.table)
 
 # Carga data barril brent
 getSymbols('DCOILBRENTEU', src='FRED')
 plot(DCOILBRENTEU)
+
+# Carga datos Gasolina 95 y Gasoleo A de 2000 a 2015
+precioCombust <- fread("precio_combustible.csv", header = TRUE)
