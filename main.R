ced70f150d456c9de9fa0e71d961bd704b902ab4
Sergio-Vazquez <svazquezpelaez@gmail.com>
2015-02-18 20:34:40 +0100

    He cambiado el nombre del campo precio del .csv, he realizado unas gráficas de líneas y lo termino con una de barras que tengo que realizar bien, dá un error.

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
 library(quantmod)
 library(ggplot2)
 library(data.table)
+library(dplyr)
+library(plyr)
 
 # Carga data barril brent
 getSymbols('DCOILBRENTEU', src='FRED')
@@ -8,3 +10,24 @@ plot(DCOILBRENTEU)
 
 # Carga datos Gasolina 95 y Gasoleo A de 2000 a 2015
 precioCombust <- fread("precio_combustible.csv", header = TRUE)
+
+# Agrupado por año y gasolina en función del precio medio en ese rango
+precioAnyo <- ddply(precioCombust, c("ANYO", "GASOLINA"), summarise, PrecioMedio = mean(PRECIO))
+
+# Otra forma de hacerlo:
+#      precioAnyo <- precioCombust %>% 
+#        group_by(ANYO, GASOLINA) %>% 
+#        summarise(medio = mean(PRECIO))
+
+# Representación rapida de los datos anteriores
+grafica1 <- qplot(ANYO, PrecioMedio, data = precioAnyo, geom = "line", colour = GASOLINA)                                        
+print(grafica1)
+
+# Ahora usando ggplot en vez de qplot:
+grafica2 <- ggplot(precioAnyo, aes(ANYO,PrecioMedio, shape = GASOLINA, colour = GASOLINA)) + layer("line")
+print(grafica2)
+
+# En grafico de barras (lo tengo que hacer bien, da un error que no llego a ver)
+b <- ggplot(precioAnyo, aes(x = ANYO, y = PrecioMedio), shape = GASOLINA)
+b + geom_bar()
+
