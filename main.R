acc42e29b6396579a851bfef2075a19ff699cffa
David-Carrasco <david.carrasco.cunado@gmail.com>
2015-02-19 14:44:02 +0100

    Creado el dataframe df.brent.oil el cual guarda la relación entre la cotizacion del brent y el precio de gasoleo y gasolina 95

--- main.R
+++ main.R
@@ -19,6 +19,34 @@ precioAnyo <- ddply(precioCombust, c("ANYO", "GASOLINA"), summarise, PrecioMedio
 #        group_by(ANYO, GASOLINA) %>% 
 #        summarise(medio = mean(PRECIO))
 
+#Funcion para obtener como objeto Date todas las fechas del dataset precioCombust
+createDate <- function(anios, meses){
+  date_month <- ifelse(nchar(mes) == 1, paste(anio, mes, sep='-0'), paste(anio, mes, sep='-'))
+  return(as.Date(paste(date_month, c('01'), sep='-'), format="%Y-%m-%d"))
+}
+
+#creamos dataset con la fecha correcta del dataset precioCombust
+#con formato aaaa-mm
+combustibles <- data.frame(fecha = format(createDate(precioCombust$ANYO, precioCombust$MES), "%Y-%m"),
+                           tipo_gasolina = precioCombust$GASOLINA,
+                           precio = precioCombust$PRECIO)
+
+#Agrupamos mensualmente el dataset del Brent y lo filtramos
+#desde el primer mes del 2000 hasta ahora y formateamos la fecha en formato aaaa-mm 
+brent_monthly <- monthlyReturn(DCOILBRENTEU, subset='2000::')
+df.brent <- data.frame(fecha = format(index(brent_monthly), "%Y-%m"),
+                       cotizacion = coredata(brent_monthly)[,1])
+
+#Join de ambos datasets por fecha
+df.brent.oil <- plyr::join(combustibles, df.brent, by='fecha', type='full')
+
+#NOTA: A la hora de pintar df.brent.oil tener en cuenta que la columna cotizacion es la variacion
+#      mensual del brent con lo cual pintarlo como cumsum cuando se pinte todo el dataframe
+
+
+
+##################################  GRAFICAS ###########################################
+
 # Representación rapida de los datos anteriores
 grafica1 <- qplot(ANYO, PrecioMedio, data = precioAnyo, geom = "line", colour = GASOLINA)                                        
 print(grafica1)
