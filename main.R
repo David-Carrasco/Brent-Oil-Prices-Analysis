fae4ae701c21630a9d31181db408f09a879b30a6
David-Carrasco <david.carrasco.cunado@gmail.com>
2015-02-20 22:37:46 +0100

    First version oil chart comparing oil and brent prices

--- main.R
+++ main.R
@@ -1,8 +1,9 @@
 library(quantmod)
 library(ggplot2)
-library(data.table)
-library(dplyr)
 library(plyr)
+library(grid)
+library(gridExtra)
+library(scales)
 
 # Carga data barril brent
 getSymbols('DCOILBRENTEU', src='FRED')
@@ -27,7 +28,7 @@ createDate <- function(anio, mes){
 
 #creamos dataset con la fecha correcta del dataset precioCombust
 #con formato aaaa-mm
-combustibles <- data.frame(fecha = format(createDate(precioCombust$ANYO, precioCombust$MES), "%Y-%m"),
+combustibles <- data.frame(fecha = createDate(precioCombust$ANYO, precioCombust$MES),
                            tipo = precioCombust$GASOLINA,
                            precio = precioCombust$PRECIO)
 
@@ -35,7 +36,7 @@ combustibles <- data.frame(fecha = format(createDate(precioCombust$ANYO, precioC
 #Agrupamos mensualmente el dataset del Brent y lo filtramos
 #desde el primer mes del 2000 hasta ahora y formateamos la fecha en formato aaaa-mm 
 brent_monthly <- to.monthly(DCOILBRENTEU['2000::'])[,c(4)]
-df.brent <- data.frame(fecha = format(index(brent_monthly), "%Y-%m"),
+df.brent <- data.frame(fecha = as.Date(format(index(brent_monthly), "%Y-%m-%d")),
                        tipo = c('brent'),
                        precio = coredata(brent_monthly)[,1])
 
@@ -44,38 +45,15 @@ df.total <- rbind(combustibles, df.brent)
 
 ##################################  GRAFICAS ###########################################
 
-# Representación rapida de los datos anteriores
-grafica1 <- qplot(ANYO, PrecioMedio, data = precioAnyo, geom = "line", colour = GASOLINA)                                        
-print(grafica1)
-
-# Ahora usando ggplot en vez de qplot:
-grafica2 <- ggplot(precioAnyo, aes(ANYO,PrecioMedio, shape = GASOLINA, colour = GASOLINA)) + layer("line")
-print(grafica2)
+graficaGasolina <- ggplot(combustibles, aes(fecha, precio, shape = tipo, colour = tipo)) + 
+  geom_line(aes(group = tipo)) + 
+  theme(legend.position = "top", axis.text.x = element_blank(), axis.title.x = element_blank()) + 
+  scale_x_date(labels = date_format("%Y"), breaks = "3 month") 
 
 
-grafica3 <- ggplot(df.total, aes(fecha,precio, shape = tipo, colour = tipo)) + layer("line")
-print(grafica3)
+graficaBrent <- ggplot(df.brent, aes(fecha, precio, shape = tipo, colour = tipo)) + 
+  geom_line(aes(group = tipo)) + 
+  theme(legend.position = "top", axis.text.x = element_text(angle=90)) + 
+  scale_x_date(labels = date_format("%Y-%b"), breaks = "3 month")
 
-
-
-# En grafico de barras (lo tengo que hacer bien, da un error que no llego a ver)
-b <- ggplot(precioAnyo, aes(x = ANYO, y = PrecioMedio), shape = GASOLINA)
-b + geom_bar()
-
-# Grafico de puntos y lineas con las 3 variables
-ggplot(df.total, aes(fecha, precio)) +
-  geom_point(aes(size=3, colour = tipo)) + geom_line()
-
-# Grafico de dispersion con la 3 variables (con line no llega a funcionar)
-ggplot(df.total, aes(fecha, precio,
-                     colour=tipo)) + geom_jitter() + facet_grid(tipo~., scale="free_y")
-
-ggplot(df.total, aes(x=fecha, y=precio,
-                     colour=tipo)) + layer(geom ="point") + facet_grid(tipo~., scale="free_y")
-
-
-# Este tipo de grafico estaria bien si saliera..
-ggplot(df.total, aes(fecha, precio)) + stat_binhex() + theme_minimal() + facet_grid(tipo~., scale="free_y")
-
-# Facet con grafica de los 3 tipos divididos
-ggplot(df.total, aes(fecha, precio,colour=tipo)) + geom_line(aes(group = tipo)) + facet_grid(tipo~., scale="free_y")
+grid.arrange(graficaGasolina, graficaBrent, ncol=1) 
